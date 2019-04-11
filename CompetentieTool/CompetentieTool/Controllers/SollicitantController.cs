af621dc8d439f4402a65228719361ccdadce1faf
Joren Debois <Joren.Debois.y9782@student.hogent.be>
2019-04-11 15:41:59 +0200

    restructured domain sollicitantcontroller met bijhorende views aangemaakt

---
+++ CompetentieTool/CompetentieTool/Controllers/SollicitantController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CompetentieTool.Controllers
+{
+    public class SollicitantController : Controller
+    {
+        public IActionResult Vacatures()
+        {
+            return View();
+        }
+
+        public IActionResult Uitnodigingen()
+        {
+            return View();
+        }
+    }
+}
