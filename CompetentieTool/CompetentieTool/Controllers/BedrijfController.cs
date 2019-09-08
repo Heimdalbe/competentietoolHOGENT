40d9c91113d781ab279d4caffa9c687d31210dcd
Joren Debois <Joren.Debois.y9782@student.hogent.be>
2019-09-08 20:42:33 +0200

    setup standard pages for vacature

--- CompetentieTool/CompetentieTool/Controllers/BedrijfController.cs
+++ CompetentieTool/CompetentieTool/Controllers/BedrijfController.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CompetentieTool.Models.IRepositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CompetentieTool.Controllers
 {
     public class BedrijfController : Controller
     {
+        private readonly IVacatureRepository _vacatureRepository;
+
         public IActionResult Index()
         {
             return View();
@@ -16,6 +19,31 @@ namespace CompetentieTool.Controllers
         public IActionResult VacaturesList()
         {
             return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        public IActionResult Delete()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+
+        public BedrijfController(IVacatureRepository vacatureRepository)
+        {
+            _vacatureRepository = vacatureRepository;
         }
     }
 }
