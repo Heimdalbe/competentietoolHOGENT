34cbd280ca610aa13f2f505295c87980635e54ce
Joren Debois <Joren.Debois.y9782@student.hogent.be>
2019-09-09 15:38:50 +0200

    created vacature viewmodel

---
+++ CompetentieTool/CompetentieTool/Models/ViewModels/VacatureViewModel.cs
@@ -0,0 +1,26 @@
+﻿using CompetentieTool.Domain;
+using CompetentieTool.Models.Domain;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompetentieTool.Models.ViewModels
+{
+    public class VacatureViewModel
+    {
+
+        [Required]
+        public String Functie { get; set; }
+
+        [Required]
+        public String Beschrijving { get; set; }
+
+        [Required]
+        public IEnumerable<Competentie> AlleCompetenties { get; set; }
+
+        [Required]
+        public IEnumerable<VacatureCompetentie> VacatureCompetenties { get; set; }
+    }
+}
